46e40576cce5d4d04cc66c516965120ae990cc59
lucianocastr <TU_EMAIL_EN_GITHUB>
2025-08-31 03:29:52 -0300

    Reverse modifica version node y boton comenzar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,12 +8,8 @@ export default function Home() {
         Este mini-sitio fue desarrollado como parte de un <strong>Proyecto de Grado</strong> en
         Ingeniería de Sistemas. Simula un flujo completo de compra de entradas con Login, Catálogo, Carrito, Checkout y Confirmación.
       </p>
-      <Link
-        to="/login"
-        className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-md shadow-md hover:shadow-lg transition"
-      >
-        Comenzar
-      </Link>
+      <a href={`${import.meta.env.BASE_URL}catalogo`} className="...">Comenzar</a>
+
     </div>
   );
 }
